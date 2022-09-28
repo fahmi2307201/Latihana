d14f8353f18b08bcfffc1e70cbc824ff37c02c0e
fahmi2307201 <113483525+fahmi2307201@users.noreply.github.com>
2022-09-28 15:43:53 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+using namespace std;
+int main () {
 
-int main() {
-  std::cout << "Hello World!\n";
+  double x, y, a, b, phi;
+
+  phi=3.1415;
+
+
+  cout << "Masukkan nilai jari-jari tabung = ";
+  cin >> x ;
+  cout << "Masukkan nilai tinggi tabung = ";
+  cin >> y ;
+
+  a = phi*x*x;
+  b = a*y;
+
+  cout << "Luas Tabung adalah " << a << endl;
+  cout << "Volume Tabung adalah " << b << endl;
+  getchar ();
+    
 }
